83ae61d97aea1bb99ea976d3306c7e26ff1bc173
freatsia <fregatsia@gmail.com>
2020-05-10 21:47:49 +0300

    added publish script for 1c WEB Services & apache

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Publish 1C bases to apache
+
+database="disporm"
+
+/opt/1C/v8.3/x86_64/webinst -apache24 -wsdir $database -dir  /var/www/html/$database -connstr "Srvr=192.168.1.19;Ref=$database;" -confPath /etc/apache2/apache2.conf
+
+systemctl restart apache2
