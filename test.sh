08588896cc7a0d0ba012b27591019023ca560727
Kirill <bugaga@local.local>
2020-05-16 13:13:51 +0300

    Added system tests

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+disk=/dev/sdh
+logs=test.log
+
+echo "### IOps test"
+#fio -readonly -name iops -rw=randread -bs=512 -runtime=20 -iodepth 32 -filename $disk -ioengine libaio -direct=1
+
+echo "### Linear write test"
+#dd if=/dev/urandom of=$disk bs=128M oflag=direct
+
+echo "### Memory test for 2-channel memory"
+#for test in {1..18}; do ramsmp -b $test -r -g 2 | grep -v RAMspeed | grep -v "per pass mode"; done |  tee $logs
+
