752dad28304c1f963e1f063214e61ef0a652c36f
div <div@local.local>
2020-05-19 23:32:52 +0300

    clean

--- sec_erase.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# hdparm --user-master u --security-set-pass pass /dev/sda
-hdparm -I /dev/sda
-# time hdparm --user-master u --security-erase pass /dev/sda
-
-# hdparm --security-disable pass
