1e60fb0ccd877d815dcf444b49accffca9006b0d
freatsia <fregatsia@gmail.com>
2020-06-02 00:30:59 +0300

    Made backup script

--- backup_pg.sh
+++ backup_pg.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-#BKPDIR="/mnt/net"
-#DATEYMD=`date "+%Y-%m-%d-%a"` // а - указывает на день недели в формате Пн, Вт, Ср...пишет по русски?
-#mkdir -p ${BKPDIR}/PostgreSQL 2>/dev/null // - можно не указывать, если предварительно создать директорию и выставить права.
-#(pg_dump mydb <"${BKPDIR}/PostgreSQL/${DATEYMD}-mydb-PostgreSQL.gz"
 
-#pg_dump -U postgres -Fc -Z9 dbname -f /mnt/net/Backup/Base/dbname.dump
+datename=`date +%Y%m%d%H%M`
+bindir="/opt/pgpro/1c-12/bin/psql"
+dstdir="/var/lib/pgadmin/storage"
+dblist="/var/lib/postgresql/scripts/db.txt"
 
-#/usr/bin/pg_dump --file "/var/lib/pgadmin/storage/account/test.backup" --host "localhost" --port "5432" --username "postgres" --no-password --verbose --format=t --blobs "database"
+server="localhost"
+port="5432"
+username="postgres"
+compress=7
 
-# 50 18 * * * sh /home/scripts/backup.sh
+cd ${bindir}
+
+# Backup globals
+pg_dumpall --globals-only > ${datename}_globals-only.sql
+
+# Backup schema
+pg_dumpall --schema-only > ${datename}_schema-only.sql
+
+# Backup databases listed in file
+for dbname in $(cat ${dblist})
+do
+        pg_dump -Fc -Z${compress} ${dbname} -f ${dstdir}/${dbname}_${datename}.sql.backup
+done
+
